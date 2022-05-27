3dd25060ff1acd5616e423925e47f8564d8213f8
Alex Winters <alex.winters@zeninternet.co.uk>
2022-05-27 20:49:53 +0100

    React rules

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit{" "}
-          <code>
-            src/App.js
-          </code>{" "}
-          and save
-          to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -24,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
-          React
+          Learn React
         </a>
       </header>
     </div>
